0b81b8e968c32ea617a03a36bae1421a130861e4
narkhederutuja <rvnarkhede06@gmail.com>
2024-11-20 18:43:47 +0530

    Add support for custom delimiters.

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,7 +1,15 @@
 class StringCalculator {
     add(numbers) {
         if (!numbers) return 0;
-        const numArray = numbers.split(/,|\n/);
+
+        let delimiter = /,|\n/;
+        if (numbers.startsWith("//")) {
+            const delimiterEnd = numbers.indexOf("\n");
+            delimiter = new RegExp(numbers.substring(2, delimiterEnd));
+            numbers = numbers.substring(delimiterEnd + 1);
+        }
+
+        const numArray = numbers.split(delimiter);
         return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
     }
 }
