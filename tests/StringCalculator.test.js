924ae5d82a1ed05c55f6f7000b28e6ab42460489
narkhederutuja <rvnarkhede06@gmail.com>
2024-11-20 18:33:07 +0530

    Add support for single numbers in the add method.

--- tests/StringCalculator.test.js
+++ tests/StringCalculator.test.js
@@ -4,3 +4,8 @@ test("should return 0 for an empty string", () => {
     const calculator = new StringCalculator();
     expect(calculator.add("")).toBe(0);
 });
+
+test("should return the number itself when one number is provided", () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add("1")).toBe(1);
+});
