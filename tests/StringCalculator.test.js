fff7563e3571a2df2a71f6c431e9a92e2d8ae8e2
narkhederutuja <rvnarkhede06@gmail.com>
2024-11-20 18:39:44 +0530

    Add support for new line delimiters.

--- tests/StringCalculator.test.js
+++ tests/StringCalculator.test.js
@@ -1,21 +1,6 @@
 const StringCalculator = require("../src/StringCalculator");
 
-test("should return 0 for an empty string", () => {
+test("should handle new lines between numbers", () => {
     const calculator = new StringCalculator();
-    expect(calculator.add("")).toBe(0);
-});
-
-test("should return the number itself when one number is provided", () => {
-    const calculator = new StringCalculator();
-    expect(calculator.add("1")).toBe(1);
-});
-
-test("should return the sum of two numbers separated by a comma", () => {
-    const calculator = new StringCalculator();
-    expect(calculator.add("1,2")).toBe(3);
-});
-
-test("should return the sum of multiple numbers", () => {
-    const calculator = new StringCalculator();
-    expect(calculator.add("1,2,3,4")).toBe(10);
+    expect(calculator.add("1\n2,3")).toBe(6);
 });
